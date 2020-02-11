dda86dc67be887121fb8e142a21707098a8abd44
jumi sanchez <jumary.sanchez@codeimmersives.com>
2020-02-11 14:01:30 -0500

    commit

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ function staples(id,url) {
     staples('image-1', 'https://i.pinimg.com/originals/0b/1c/23/0b1c2307c83e1ebdeed72e41b9a058ad.gif')
     staples('image-2', 'https://media1.tenor.com/images/c13ca8c474c7ef4b0a959535aa8cc2e3/tenor.gif')
     staples('image-3', 'https://d24bnpykhxwj9p.cloudfront.net/s3file/s3fs-public/users1/2017-02/Thu/giphy_1.gif')
+    
+
+    function drawings(){
+
+    return  document.querySelector('#a').style.textDecoration= 'line-through'
+    }
+    drawings()
+    
+
+    function rem(){
+        return document.querySelector('#arguments').lastElementChild.remove()
+        
+    }
+    rem()
+    rem()
